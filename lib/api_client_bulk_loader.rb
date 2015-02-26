7e8f8666ffd10bd64a6dcd6d6dcc0742e69bda66
Alex Ross <alex@avvo.com>
2015-02-26 14:11:47 -0800

    smaller activesupport footprint

--- lib/api_client_bulk_loader.rb
+++ lib/api_client_bulk_loader.rb
@@ -1,4 +1,4 @@
-require 'activesupport'
+require 'active_support/concern'
 require 'json_api_client'
 
 require "api_client_bulk_loader/api_client_bulk_loader"
--- lib/api_client_bulk_loader/version.rb
+++ lib/api_client_bulk_loader/version.rb
@@ -1,3 +1,3 @@
 module ApiClientBulkLoader
-  VERSION = "0.1.4"
+  VERSION = "0.1.5"
 end
