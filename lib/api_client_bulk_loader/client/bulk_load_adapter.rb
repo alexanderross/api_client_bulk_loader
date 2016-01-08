22707fb5213c9293ed1bbc95106e63abe216866a
Alex Ross <alex@avvo.com>
2016-01-07 18:34:38 -1000

    added base adapter class

---
+++ lib/api_client_bulk_loader/client/bulk_load_adapter.rb
@@ -0,0 +1,30 @@
+module ApiClientBulkLoader
+  module Client
+    #Adapts into the bulk loader. Mainly useful for storing the details of the bulk call.
+    class BulkLoadAdapter
+      attr_reader :attribute, :autoload, :values_from, :limit
+
+      def initialize(attribute = nil, values_from = nil, autoload = false, is_has_one = false, limit = nil)
+        @attribute = attribute
+        @autoload = autoload
+        @values_from = values_from
+        @type_from = type_from
+        @has_one = is_has_one
+        @limit = limit
+      end
+
+      def bulk_loader
+        RequestStore.store[:bulk_loader] ||= ApiClientBulkLoader::Client::Loader.new
+        return RequestStore.store[:bulk_loader]
+      end
+
+      def push
+        raise Exception.new("Child class should define push!")
+      end
+
+      def fetch
+        raise Exception.new("Child class should define fetch!")
+      end
+    end
+  end
+end
